1575b1492c9df2736f23b606c837f3089c249e6c
Spencer Haney <theOfficialSkara@gmail.com>
2018-06-29 01:30:55 -0400

    Implement player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    public float acceleration;
+    public float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update ()
+    {
+        var targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 force = speed * targetVelocity - rb.velocity;
+        rb.AddForce(force * acceleration);
+    }
+}
