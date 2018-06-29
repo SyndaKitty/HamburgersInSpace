3cef3c5814d78ca50d9614b515415aa16ee75f71
Spencer Haney <theOfficialSkara@gmail.com>
2018-06-29 01:12:28 -0400

    Main menu pointers

---
+++ Assets/Scripts/ButtonHover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject Pointer;
+    SpriteRenderer renderer;
+
+    void Awake()
+    {
+        renderer = Pointer.GetComponent<SpriteRenderer>();
+        renderer.enabled = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        renderer.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        renderer.enabled = false;
+    }
+}
