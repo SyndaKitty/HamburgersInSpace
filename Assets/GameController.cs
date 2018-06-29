25ca1673255648ff48c2aa236a425ed1ff1f7bb4
Spencer Haney <theOfficialSkara@gmail.com>
2018-06-28 22:56:54 -0400

    Added main menu

---
+++ Assets/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Button StartButton;
+    public Button QuitButton;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        StartButton.onClick.AddListener(StartGame);
+        QuitButton.onClick.AddListener(QuitGame);
+    }
+
+    void Start ()
+    {
+        
+    }
+    
+    void Update ()
+    {
+        
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
