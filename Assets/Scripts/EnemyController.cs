c814842c8f63c32e83a6f0d86e504a3b7de3e866
Spencer Haney <theOfficialSkara@gmail.com>
2018-06-30 12:29:48 -0400

    Start implementing enemy navigation

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public bool Attack;
+    public int StartingBurstAmount;
+    public float StartingBurstTimeout;
+
+    EnemyNavigator navigator;
+    GameObject player;
+    Rigidbody2D rb;
+    Unit unit;
+    float burstTimeout;
+    float burstAmount;
+    bool bursting;
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        navigator = GetComponent<EnemyNavigator>();
+        rb = GetComponent<Rigidbody2D>();
+        unit = GetComponent<Unit>();
+        unit.Initialize(OnDeath);
+        burstTimeout = StartingBurstTimeout;
+        burstAmount = StartingBurstAmount;
+    }
+
+    void Update()
+    {
+        unit.CustomUpdate();
+
+        if (!Attack) return;
+
+        if (bursting && burstAmount <= 0)
+        {
+            burstAmount = StartingBurstAmount;
+            burstTimeout = StartingBurstTimeout;
+            bursting = false;
+        }
+
+        if (bursting && player != null)
+        {
+            Vector2 target = player.transform.position;
+            if (unit.Shoot(target))
+            {
+                burstAmount--;
+            }
+        }
+        else if (burstTimeout < 0)
+        {
+            burstTimeout = 0;
+            bursting = true;
+        }
+        else
+        {
+            burstTimeout -= Time.deltaTime;
+        }
+
+        if (navigator != null)
+        {
+            ControlNavigation();
+        }
+    }
+
+    void OnDeath()
+    {
+        Destroy(gameObject);
+    }
+
+    void ControlNavigation()
+    {
+        if (navigator.State == NavigationState.Stationary) return;
+        if (player == null)
+        {
+            navigator.State = NavigationState.Neutral;
+        }
+        else
+        {
+            float playerDistance = ( player.transform.position - transform.position).magnitude;
+            if (playerDistance < navigator.ProximityRadius.x)
+            {
+                navigator.State = NavigationState.FleeingPlayer;
+            }
+            else if (playerDistance > navigator.ProximityRadius.y)
+            {
+                navigator.State = NavigationState.SeekingPlayer;
+            }
+            else if (navigator.State == NavigationState.ShimmyLeft)
+            {
+                navigator.ShimmyTime -= Time.deltaTime;
+                if (navigator.ShimmyTime <= 0)
+                {
+                    navigator.State = NavigationState.ShimmyRight;
+                    navigator.ShimmyTime = Random.Range(navigator.ShimmyRange.x, navigator.ShimmyRange.y);
+                }
+            }
+            else if (navigator.State == NavigationState.ShimmyRight)
+            {
+                navigator.ShimmyTime -= Time.deltaTime;
+                if (navigator.ShimmyTime <= 0)
+                {
+                    navigator.State = NavigationState.ShimmyLeft;
+                    navigator.ShimmyTime = Random.Range(navigator.ShimmyRange.x, navigator.ShimmyRange.y);
+                }
+            }
+            else
+            {
+                int leftRight = Random.Range(0, 2);
+                if (leftRight == 0)
+                {
+                    navigator.State = NavigationState.ShimmyLeft;
+                }
+                else
+                {
+                    navigator.State = NavigationState.ShimmyRight;
+                }
+                navigator.ShimmyTime = Random.Range(navigator.ShimmyRange.x, navigator.ShimmyRange.y);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyNavigator.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public enum NavigationState
+{
+    SeekingPlayer,
+    FleeingPlayer,
+    Neutral,
+    ShimmyLeft,
+    ShimmyRight,
+    Stationary,
+}
+
+public class EnemyNavigator : MonoBehaviour
+{
+    public NavigationState State;
+    public Vector2 StationaryPosition;
+    public float MaxForce;
+    public float Acceleration;
+    public Vector2 StartingProximityRadius;
+    public Vector2 ProximityRadius;
+    public Vector2 ShimmyRange;
+    public float ShimmyTime;
+    public float ShimmyAcceleration;
+
+    Rigidbody2D rb;
+    GameObject player;
+    Vector2 force;
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        StationaryPosition = transform.position;
+        ProximityRadius = StartingProximityRadius;
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void LateUpdate()
+    {
+        Vector2 force = Vector2.zero;
+        if (player == null) State = NavigationState.Neutral;
+        switch (State)
+        {
+            case NavigationState.SeekingPlayer:
+            {
+                var targetVelocity = player.transform.position - transform.position;
+                force = (Vector2)targetVelocity - rb.velocity;
+                break;
+            }
+            case NavigationState.FleeingPlayer:
+            {
+                var targetVelocity = transform.position - player.transform.position;
+                force = (Vector2)targetVelocity - rb.velocity;
+                break;
+            }
+            case NavigationState.Neutral:
+            {
+                var targetVelocity = Vector2.zero;
+                force = (Vector2)targetVelocity - rb.velocity;
+                break;
+            }
+            case NavigationState.ShimmyLeft:
+            {
+                var toPlayer = player.transform.position - transform.position;
+                toPlayer.Normalize();
+                var shimmyVector = Quaternion.Euler(0, 0, -90) * toPlayer * ShimmyAcceleration;
+                force = (Vector2)shimmyVector - rb.velocity;
+                break;
+            }
+            case NavigationState.ShimmyRight:
+            {
+                var toPlayer = player.transform.position - transform.position;
+                toPlayer.Normalize();
+                var shimmyVector = Quaternion.Euler(0, 0, 90) * toPlayer * ShimmyAcceleration;
+                force = (Vector2)shimmyVector - rb.velocity;
+                break;
+            }
+            case NavigationState.Stationary:
+            {
+                var targetVelocity = StationaryPosition - (Vector2)transform.position;
+                force = (Vector2)targetVelocity - rb.velocity;
+                break;
+            }
+        }
+
+        force *= Acceleration;
+        if (force.magnitude > MaxForce)
+        {
+            force = force.normalized * MaxForce;
+        }
+        rb.AddForce(force);
+    }
+}
