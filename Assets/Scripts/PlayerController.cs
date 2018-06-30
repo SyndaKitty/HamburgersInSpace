304c953261e9d6e5e9a97dcb5461c07dd5851b1b
Spencer Haney <theOfficialSkara@gmail.com>
2018-06-30 01:33:16 -0400

    Start implement shielding

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public GameObject PicklePrefab;
     public float RateOfFire = .3f;
 
+
     Rigidbody2D rb;
     Unit unit;
     CircleCollider2D collider;
@@ -30,19 +31,26 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        unit.CustomUpdate();
+
         var targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector2 rightAnalog = new Vector2(Input.GetAxisRaw("RAnalogX"), Input.GetAxisRaw("RAnalogY"));
-
-        Debug.Log(rightAnalog);
+        var mouseTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         Vector2 force = Speed * targetVelocity - rb.velocity;
         rb.AddForce(force * Acceleration);
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(1))
         {
-            var target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            // TODO
-            unit.Shoot(collider, target);
+            unit.Shield(mouseTarget);
+        }
+        else if ((Input.GetAxisRaw("TriggerL") > 0 || Input.GetAxisRaw("TriggerR") > 0) && rightAnalog != Vector2.zero)
+        {
+            unit.Shield(rightAnalog.normalized + (Vector2)transform.position);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            unit.Shoot(collider, mouseTarget);
         }
         else if (rightAnalog != Vector2.zero)
         {
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -10,21 +10,45 @@ public class Unit : MonoBehaviour
     public float PickleVelocity;
     public float RateOfFire;
     public float countDown;
+    public float BunShiledDistance;
+    public Sprite FullBurgerSprite;
+    public Sprite SplitBurgerSprite;
+    public GameObject BunShiledPrefab;
+    public Vector2 BunShiledOffset;
+
     Action OnDeathCallback;
     GameObject picklePrefab;
+    SpriteRenderer sr;
+    bool shielding;
+    GameObject bunShiled;
 
     public void Initialize(float health, GameObject picklePrefab, float rateOfFire, Action OnDeathCallback = null)
     {
+        sr = GetComponent<SpriteRenderer>();
         this.Health = health;
         this.picklePrefab = picklePrefab;
         this.OnDeathCallback = OnDeathCallback;
         this.RateOfFire = rateOfFire;
+        shielding = false;
+        sr.sprite = FullBurgerSprite;
+
+        bunShiled = Instantiate(BunShiledPrefab);
+        bunShiled.SetActive(false);
     }
 
-    void Update()
+    public void CustomUpdate()
     {
+        SetShielding(false);
         countDown -= Time.deltaTime;
         if (countDown < 0) countDown = 0;
+    }
+
+    public void Shield(Vector2 target)
+    {
+        SetShielding(true);
+        var targetDifference = target - (Vector2)transform.position;
+        bunShiled.transform.position = targetDifference.normalized * BunShiledDistance + (Vector2)transform.position + BunShiledOffset;
+        bunShiled.transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(targetDifference.y, targetDifference.x) * Mathf.Rad2Deg - 90, Vector3.forward);
     }
 
     public void Shoot(Collider2D sourceCollider, Vector3 target)
@@ -74,5 +98,19 @@ public class Unit : MonoBehaviour
     public void Die()
     {
         OnDeathCallback();
+    }
+
+    void SetShielding(bool shielding)
+    {
+        this.shielding = shielding;
+        if (shielding)
+        {
+            sr.sprite = SplitBurgerSprite;
+        }
+        else
+        {
+            sr.sprite = FullBurgerSprite;
+        }
+        bunShiled.SetActive(shielding);
     }
 }
